c2fe3c217c3dbb6bf6e7296a36a74a17761071ef
Johann Pouponnot <johannpouponnot@mbp-de-johann.lan>
2022-02-17 11:33:03 +0100

    feat: paginate on home too

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,14 +5,20 @@ import Content from "../components/List/Content/Content";
 const Home = ({ search }) => {
   const [characters, setCharacters] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
     fetchData(
-      `http://localhost:4000/characters?name=${search}`,
+      `http://localhost:4000/characters?name=${search}&page=${page}`,
       setCharacters,
       setIsLoading
     );
-  }, [search]);
-  return isLoading ? <span>en chargement</span> : <Content data={characters} />;
+  }, [search, page]);
+  return isLoading ? (
+    <span>en chargement</span>
+  ) : (
+    <Content data={characters} page={page} setPage={setPage} />
+  );
 };
 
 export default Home;
